6d350fdab8c71099fe27836c33241c146dbc62c2
RaniaAwad <52722859+RaniaAwad@users.noreply.github.com>
2019-07-09 15:55:39 -0700

    Add files via upload

---
+++ youaredeadtome.js
@@ -0,0 +1,3 @@
+<button id="sadboigif" onclick="document.getElementById('threehundred').src='https://media2.giphy.com/media/3o6wrvdHFbwBrUFenu/giphy.gif'  "> wanna feel happy? </button>
+
+<img id="threehundred" src= 'https://media0.giphy.com/media/hnBAztravXiYo/giphy.gif' style="height:300px">
